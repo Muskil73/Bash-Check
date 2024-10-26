5885e5fa9916d1af14773941517a442aef831d7e
Muskil73 <muskilgnanavathy73@gmail.com>
2024-10-26 12:48:37 +0530

    new branch

--- src/main/java/GitBash.java
+++ src/main/java/GitBash.java
@@ -5,6 +5,7 @@ public class GitBash {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to GitBash");
 		System.out.println("Muhesh na Mass!!!");
+		System.out.println("We are All MASS!!!");
 
 
 	}
